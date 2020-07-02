c35bc1a1cc00bec67630d72100ce466b1d63c567
Arijit Dey <arijid79@gmail.com>
2020-07-02 18:05:54 +0530

    Added a basic user setup interface with no widgets

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use gtk;
+use gtk::prelude::*;
+
 fn main() {
-    println!("Hello World!");
+    let e = gtk::init();
+
+    match e {
+        Ok(_) => {},
+        Err(_) => {}
+    }
+
+    let source = include_str!("../setup.glade");
+    let builder = gtk::Builder::new_from_string(source);
+
+    let window: gtk::Window = builder.get_object("window1").unwrap();
+    window.show_all();
+
+    gtk::main();
 }
