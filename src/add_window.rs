13fc86829ff9c74a26b4063c005e409c391a3a27
Arijit Dey <arijid79@gmail.com>
2020-07-26 15:22:46 +0530

    Added comments in add_window.rs

--- src/add_window.rs
+++ src/add_window.rs
@@ -21,14 +21,21 @@ fn get_word_count(buff: &gtk::TextBuffer) -> usize {
     split_string.count()
 }
 
+// Open the confirm quit dialog
 fn config_confirm_quit(src: String, parent: gtk::Window) {
+    // Generate builder from source
     let b = gtk::Builder::new_from_string(&src);
+    // Get the window and the buttons
     let win: gtk::Window = b.get_object("confirm_quit").unwrap();
     let close_dialog: gtk::Button = b.get_object("close_dialog").unwrap();
     let close_parent: gtk::Button = b.get_object("close_window").unwrap();
+    // Set the transient for the dialog box
     win.set_transient_for(Some(&parent));
+    // Show the dialog box
     win.show_all();
+    // Close the dialog if cancel is pressed
     close_dialog.connect_clicked(move |_| win.destroy());
+    // Destroy the add note window if don't save is pressed
     close_parent.connect_clicked(move |_| parent.destroy());
 }
 
@@ -56,9 +63,11 @@ pub fn init_add(path: String, notes: gtk::ListStore) {
     let char_count: gtk::Label = b.get_object("char_count").unwrap();
     let word_count: gtk::Label = b.get_object("word_count").unwrap();
     let line_count: gtk::Label = b.get_object("line_count").unwrap();
+    // Generate the filename where note would be stored
     let filen = gen_filename(&path);
-    // Generate the file path
+    // Variable that keeps a check whether the note is saved or not
     let saved = Rc::new(RefCell::new(true));
+    // Make reference to be used in closures
     let save_c1 = saved.clone();
     let save_c2 = saved.clone();
 
@@ -70,6 +79,7 @@ pub fn init_add(path: String, notes: gtk::ListStore) {
         line_count.set_text(&format!("Lines: {}", tb.get_line_count()));
         line_no.set_text(&format!("Line: {}", text_iter.get_line()));
         col_no.set_text(&format!("Col: {}", text_iter.get_line_offset()));
+        // There are unsaved changes
         *saved.borrow_mut() = false;
     });
     // Save button functionality
@@ -93,10 +103,12 @@ pub fn init_add(path: String, notes: gtk::ListStore) {
     });
 
     win.connect_delete_event(move |win, _| -> glib::signal::Inhibit {
+        // If there are unsaved changes, open up the confirm quit window
         if !save_c2.borrow().to_owned() {
             config_confirm_quit(src.to_string(), win.to_owned());
             glib::signal::Inhibit(true)
         } else {
+            // The delete event has not been handled, show use the default destroy event
             glib::signal::Inhibit(false)
         }
     });
